531ca75b16c15c08e58f7c2560b442fd8df9831b
Apple <home@Selman.local>
2019-01-07 10:29:15 +0300

    Initial Commit

---
+++ To-Do-List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To-Do-List
+//
+//  Created by Apple on 7.01.2019.
+//  Copyright © 2019 Selman Comoglu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
